2856e7eff1e26f14a99c56065fa4c06d5ab2c62f
LifeStly <tattonman001@gmail.com>
2020-10-10 23:48:01 +0700

    set Layout

---
+++ app/src/main/java/com/example/moveproject/MovieDetail/SingleMovie.kt
@@ -0,0 +1,12 @@
+package com.example.moveproject.MovieDetail
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.moveproject.R
+
+class SingleMovie : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_single_movie)
+    }
+}
